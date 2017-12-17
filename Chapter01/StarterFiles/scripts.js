56f7b33b429c994031d8a5e4c53cba09a113d954
Piotr Szklanko <piotr.szklanko@toreforge.com>
2017-12-17 23:01:10 +0100

    check and remove notes

--- Chapter01/StarterFiles/scripts.js
+++ Chapter01/StarterFiles/scripts.js
@@ -8,7 +8,7 @@ class ToDoClass {
         this.loadTasks();
     }
 
-    loadTask() {
+    loadTasks() {
         let tasksHtml = this.tasks.reduce((html, task, index) => html +=
         this.generateTaskHtml(task, index), '');
         document.getElementById('taskList').innerHTML = tasksHtml;
@@ -18,14 +18,37 @@ class ToDoClass {
         return `
         <li class="list-group-item checkbox">
             <div class="row">
-                <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checbox">
-                    <label><input id="toggleTaskStatus" type="checbox"
+                <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+                    <label><input id="toggleTaskStatus" type="checkbox"
                     onchange="toDo.toggleTaskStatus(${index})" value="" class=""
-                    ${task.isComplete?'checked':''}</label>label>
+                    ${task.isComplete?'checked':''}></label>
                 </div>
-        `
+                <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete ? 'complete' : ''}">
+                    ${task.task}
+                </div>
+                <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+                    <a class="" href="/" onClick="toDo.deleteTask(event, ${index})">
+                        <i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i>
+                    </a>
+                </div>
+            </div>
+        </li>               
+        `;
+    }
+
+    toggleTaskStatus(index) {
+        this.tasks[index].isComplete = !this.tasks[index].isComplete;
+        this.loadTasks();
+    }
+
+    deleteTask(event, index) {
+        event.preventDefault();
+        this.tasks.splice(index, 1);
+        this.loadTasks();
+
     }
 }
+
 let toDo;
 window.addEventListener('load', () => {
     toDo = new ToDoClass();
